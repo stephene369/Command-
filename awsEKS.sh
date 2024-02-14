3ea6ef22ff69ebc4cd80b408da398cb918b35bbe
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-14 11:01:21 +0100

    Create awsEKS.sh

---
+++ awsEKS.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+## install AWS Console 
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -u awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+which aws
+ls -l /usr/local/bin/aws
+aws --version
