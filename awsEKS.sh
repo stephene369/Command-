0a92440df4a1358fc1d382d5857e8e28c51f0b3a
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-14 11:25:39 +0100

    Update awsEKS.sh

--- awsEKS.sh
+++ awsEKS.sh
@@ -3,8 +3,31 @@
 ## install AWS Console 
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
 unzip -u awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 which aws
 ls -l /usr/local/bin/aws
 aws --version
+
+##################################### AWS configuration 
+export AWS_AID=AKIAVNEMEULAQJOIDKHJ
+export AWS_AKEY=v1szXfdaIyrbHkpxPMr4C6S+jXboc1jwoTjSXMeG
+export AWS_AR=us-west-2
+
+aws configure
+
+## verifi user identy 
+aws sts get-caller-identity
+
+#################################### Install EKS 
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
